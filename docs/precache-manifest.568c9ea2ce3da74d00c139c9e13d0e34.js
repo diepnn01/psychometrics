00857f3e05d8f267b0b1f9568714165fcc71654b
nam-pg <nam.cn@propertyguru.com.sg>
2020-07-09 16:30:59 +0700

    update demo

---
+++ docs/precache-manifest.568c9ea2ce3da74d00c139c9e13d0e34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ffb0f50811b0741ced1aec0b43bd95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b33ffe9b9e301f002052",
+    "url": "./static/css/main.953a9ad7.chunk.css"
+  },
+  {
+    "revision": "1f654993c18c1281a00c",
+    "url": "./static/js/2.fd1bfcb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.fd1bfcb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b33ffe9b9e301f002052",
+    "url": "./static/js/main.7d761919.chunk.js"
+  },
+  {
+    "revision": "08b56b45bac3a73fa909",
+    "url": "./static/js/runtime-main.8ea472da.js"
+  }
+]);
