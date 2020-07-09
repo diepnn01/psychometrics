0dcaa32dde658ec8a0b02d0d17029c8a372f74bc
nam-pg <nam.cn@propertyguru.com.sg>
2020-07-09 16:20:22 +0700

    fixed issue not work on github page

---
+++ docs/precache-manifest.a84a80e9792f0ade740d92b7a1380db9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eabb0739a290d54700846b0bd3ca8d8",
+    "url": "./docs/index.html"
+  },
+  {
+    "revision": "89d889118a20a85440e3",
+    "url": "./docs/static/css/main.cf26436c.chunk.css"
+  },
+  {
+    "revision": "1f654993c18c1281a00c",
+    "url": "./docs/static/js/2.fd1bfcb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./docs/static/js/2.fd1bfcb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89d889118a20a85440e3",
+    "url": "./docs/static/js/main.beb4c067.chunk.js"
+  },
+  {
+    "revision": "6b0bba130c177f76ca75",
+    "url": "./docs/static/js/runtime-main.e1a66262.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/psychometrics/precache-manifest.9aa254d48deb6009fde20c7abc3951b4.js"
+  "./docs/precache-manifest.a84a80e9792f0ade740d92b7a1380db9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/psychometrics/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./docs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
